840fbf93424e0908dccd7f6eaa14d43b9bdd78e1
GalangRmd <galangramad1113@gmail.com>
2023-09-19 13:16:39 +0700

    Push Kedua

--- akun.php
+++ akun.php
@@ -60,14 +60,7 @@ if (isset($_POST['ubah'])) {
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0"><i class="fas fa-user-cog"></i> Data Akun</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item active">Data Akun</li>
-                    </ol>
-                </div><!-- /.col -->
+                
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
@@ -80,61 +73,29 @@ if (isset($_POST['ubah'])) {
             <!-- Main content -->
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Tabel Data Akun</h3>
+                
+                            <!-- tampil seluruh data -->
+                            
+                               
+                    <h3 class="card-title">Hallo </h3>
+                    
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body">
-                    <?php if ($_SESSION['level'] == 1) : ?>
-                        <button type="button" class="btn btn-primary btn-sm mb-1" data-toggle="modal" data-target="#modalTambah"><i class="fas fa-plus"></i> Tambah</button>
-                    <?php endif; ?>
-
-                    <table id="example2" class="table table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Nama</th>
-                                <th>Username</th>
-                                <th>Email</th>
-                                <th>Password</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <?php $no = 1; ?>
-                            <!-- tampil seluruh data -->
-                            <?php if ($_SESSION['level'] == 1) : ?>
-                                <?php foreach ($data_akun as $akun) : ?>
-                                    <tr>
-                                        <td><?= $no++; ?></td>
-                                        <td><?= $akun['nama']; ?></td>
-                                        <td><?= $akun['username']; ?></td>
-                                        <td><?= $akun['email']; ?></td>
-                                        <td>Password Ter-enkripsi</td>
-                                        <td class="text-center">
-                                            <button type="button" class="btn btn-success mb-1" data-toggle="modal" data-target="#modalUbah<?= $akun['id_akun']; ?>"><i class="fas fa-edit"></i> Ubah</button>
-
-                                            <button type="button" class="btn btn-danger mb-1" data-toggle="modal" data-target="#modalHapus<?= $akun['id_akun']; ?>"><i class="fas fa-trash-alt"></i> Hapus</button>
-                                        </td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            <?php else : ?>
-                                <!-- tampil data berdasarkan user login -->
-                                <?php foreach ($data_bylogin as $akun) : ?>
-                                    <tr>
-                                        <td><?= $no++; ?></td>
-                                        <td><?= $akun['nama']; ?></td>
-                                        <td><?= $akun['username']; ?></td>
-                                        <td><?= $akun['email']; ?></td>
-                                        <td>Password Ter-enkripsi</td>
-                                        <td class="text-center">
-                                            <button type="button" class="btn btn-success mb-1" data-toggle="modal" data-target="#modalUbah<?= $akun['id_akun']; ?>">Ubah</button>
-                                        </td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            <?php endif; ?>
-                        </tbody>
-                    </table>
+                    <div class=" max-w-7xl mx-auto sm:px-6 lg:px-8  ">  
+                        <div class="ciah shadow-sm sm:rounded-lg" style=" width: 100%; height: 100vh; background-image: url('https://media.licdn.com/dms/image/D5605AQGeUxt6kfzs5A/feedshare-thumbnail_720_1280/0/1693883976893?e=2147483647&v=beta&t=knBoP755Kd0yNKGUvVK1IuCycgebBBLGNfJy0CFhI38'); backdrop-filter: blur(5px); background-size: cover;  height: 50vh ;    background-size: cover ;    background-position: center ;    background-repeat: no-repeat ;">  
+                            <div> 
+                            <center>
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal" style='margin-top: 150px;'>
+                                keluar
+                                </button>
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal" style='margin-top: 150px;'>
+                                haha
+                                </button>
+                            </center>
+                            </div>  
+                        </div>  
+                    </div>  
                 </div>
             </div>
         </div><!-- /.container-fluid -->
@@ -142,137 +103,6 @@ if (isset($_POST['ubah'])) {
     <!-- /.content -->
 </div>
 
-<!-- Modal Tambah -->
-<div class="modal fade" id="modalTambah" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-plus"></i> Tambah Akun</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="" method="post">
-                    <div class="form-group">
-                        <label for="nama">Nama</label>
-                        <input type="text" name="nama" id="nama" class="form-control" required>
-                    </div>
 
-                    <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" name="username" id="username" class="form-control" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" id="email" class="form-control" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" id="password" class="form-control" required minlength="6">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="level">Level</label>
-                        <select name="level" id="level" class="form-control" required>
-                            <option value="">-- pilih level --</option>
-                            <option value="1">Super Admin</option>
-                            <option value="2">Admin</option>
-                            <option value="3">Karyawan</option>
-                        </select>
-                    </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Kembali</button>
-                <button type="submit" name="tambah" class="btn btn-primary">Tambah</button>
-            </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-<!-- Modal Ubah -->
-<?php foreach ($data_akun as $akun) : ?>
-    <div class="modal fade" id="modalUbah<?= $akun['id_akun']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-edit"></i> Ubah Akun</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="" method="post">
-                        <input type="hidden" name="id_akun" value="<?= $akun['id_akun']; ?>">
-
-                        <div class="form-group">
-                            <label for="nama">Nama</label>
-                            <input type="text" name="nama" id="nama" class="form-control" value="<?= $akun['nama']; ?>" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="username">Username</label>
-                            <input type="text" name="username" id="username" class="form-control" value="<?= $akun['username']; ?>" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" name="email" id="email" class="form-control" value="<?= $akun['email']; ?>" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password">Password <small>(Masukkan password baru/lama)</small></label>
-                            <input type="password" name="password" id="password" class="form-control" required minlength="6">
-                        </div>
-
-                        <?php if ($_SESSION['level'] == 1) : ?>
-                            <div class="form-group">
-                                <label for="level">Level</label>
-                                <select name="level" id="level" class="form-control" required>
-                                    <?php $level = $akun['level']; ?>
-                                    <option value="1" <?= $level == '1' ? 'selected' : null ?>>Admin</option>
-                                    <option value="2" <?= $level == '2' ? 'selected' : null ?>>Operator Barang</option>
-                                    <option value="3" <?= $level == '3' ? 'selected' : null ?>>Operator Mahasiswa</option>
-                                </select>
-                            </div>
-                        <?php else : ?>
-                            <input type="hidden" name="level" value="<?= $akun['level']; ?>">
-                        <?php endif; ?>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Kembali</button>
-                    <button type="submit" name="ubah" class="btn btn-primary">Ubah</button>
-                </div>
-                </form>
-            </div>
-        </div>
-    </div>
-<?php endforeach; ?>
-
-<!-- Modal Hapus -->
-<?php foreach ($data_akun as $akun) : ?>
-    <div class="modal fade" id="modalHapus<?= $akun['id_akun']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-trash-alt"></i> Hapus Akun</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p>Yakin Ingin Menghapus Data Akun : <?= $akun['nama']; ?> .?</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Kembali</button>
-                    <a href="hapus-akun.php?id_akun=<?= $akun['id_akun']; ?>" class="btn btn-danger">Hapus</a>
-                </div>
-            </div>
-        </div>
-    </div>
-<?php endforeach; ?>
 
 <?php include 'layout/footer.php'; ?>
