7e9b0c6ecae66123ec7dd63647e00b2ca3d617a0
daffaaptar <daffaaptapratama@gmail.com>
2023-09-24 13:31:38 +0700

    Menambahkan Dashboard

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ if (isset($_POST['login'])) {
             $_SESSION['level']      = $hasil['level'];
 
             // jika login benar arahkan ke file index.php
-            header("Location: absen.php");
+            header("Location: index.php");
             exit;
         }
     }
